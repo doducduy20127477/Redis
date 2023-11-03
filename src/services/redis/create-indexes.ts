d1a5174a671b007a93f6049a5a4236cca43d4a59
Đỗ Đức Duy <90464550+doducduy20127477@users.noreply.github.com>
2023-11-03 10:33:53 +0700

    Initial commit

---
+++ src/services/redis/create-indexes.ts
@@ -0,0 +1,28 @@
+import { SchemaFieldTypes } from 'redis';
+import { client } from './client';
+import { itemsIndexKey, itemsKey } from '$services/keys';
+
+export const createIndexes = async () => {
+	const indexes = await client.ft._list();
+	const exists = indexes.find((index) => index === itemsIndexKey());
+
+	if (exists) {
+		return;
+	}
+
+	return client.ft.create(
+		itemsIndexKey(),
+		{
+			name: {
+				type: SchemaFieldTypes.TEXT
+			},
+			description: {
+				type: SchemaFieldTypes.TEXT
+			}
+		},
+		{
+			ON: 'HASH',
+			PREFIX: itemsKey('')
+		}
+	);
+};
